a4f78da041af844eee79499a67695cf17f4cc08d
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-05-12 22:17:14 -0400

    f01.js cleanup

--- ex01/js/f01.js
+++ ex01/js/f01.js
@@ -1,18 +1,18 @@
 // dateObjectFromString
 window.scope02 = {};
-scope02.dateObjectFromString = function( s ) {
-        console.log("s is a " + typeof s + ": " + s + " - " + s );
-        var arydate = s.split("-");
-       //  console.log("--- arydate");
+scope02.dateObjectFromString = function (s) {
+    console.log("s is a " + typeof s + ": " + s + " - " + s);
+    var arydate = s.split("-");
+    //  console.log("--- arydate");
 
-       //  create date object from parsed date
-       //  var aryDateObject = new Date( arydate[0], arydate[1]-1, arydate[2]);
-        var date = new Date();
-        var year = parseInt(arydate[0]);
-        var month = parseInt(arydate[1]);
-        var day = parseInt(arydate[2]);
-        console.log(year + "-" + month +"-" + day + " (" + typeof year + "-" + typeof month + "-" + typeof day + ")");
-        date.setFullYear( year, month, day);
+    //  create date object from parsed date
+    //  var aryDateObject = new Date( arydate[0], arydate[1]-1, arydate[2]);
+    var date = new Date();
+    var year = parseInt(arydate[0]);
+    var month = parseInt(arydate[1]);
+    var day = parseInt(arydate[2]);
+    console.log(year + "-" + month + "-" + day + " (" + typeof year + "-" + typeof month + "-" + typeof day + ")");
+    date.setFullYear(year, month, day);
 
-        return date;
-    };
+    return date;
+};
