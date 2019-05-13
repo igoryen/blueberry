bebde546e0ebf4b81be289676253a37d641c5e6c
Igor Entaltsev <igor.yentaltsev@gmail.com>
2019-05-12 22:16:17 -0400

    f02.js cleanup

--- ex01/js/f02.js
+++ ex01/js/f02.js
@@ -1,17 +1,17 @@
 window.scope02b = {};
-scope02b.compare = function (x,y) {
-        // var a = dateObjectFromString(x["from"]).getTime();
-        // var b = dateObjectFromString(y["from"]).getTime();
-        // var that = this;
+scope02b.compare = function (x, y) {
+    // var a = dateObjectFromString(x["from"]).getTime();
+    // var b = dateObjectFromString(y["from"]).getTime();
+    // var that = this;
 
-        var a = moment(x["from"]).format("YYYY");
-        var b = moment(y["from"]).format("YYYY");
+    var a = moment(x["from"]).format("YYYY");
+    var b = moment(y["from"]).format("YYYY");
 
-        console.log(" >>>>> " + x["truefrom"] + " " + x["title"]);
+    console.log(" >>>>> " + x["truefrom"] + " " + x["title"]);
 
-        if (a < b)
-            return -1;
-        if (a > b)
-            return 1;
-        return 0;
-    };
+    if (a < b)
+        return -1;
+    if (a > b)
+        return 1;
+    return 0;
+};
